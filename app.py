30b3bd8124572e40f04476442d44edb893b67623
Tina Huang <t42huang@gmail.com>
2020-03-18 07:26:13 -0400

    add a new route to a new page

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def hello():
     return "Hello World!"
 
+@app.route('/<name>')
+def hi_there(name):
+    return "Hi {}!".format(name)
+
 if __name__ == '__main__':
     app.run()
