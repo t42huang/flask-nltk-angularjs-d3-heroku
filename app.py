070203f8a9dd64153c7100ea4023da3761dd0212
Tina Huang <t42huang@gmail.com>
2020-03-19 08:21:00 -0400

    fix indentation

--- app.py
+++ app.py
@@ -38,42 +38,42 @@ def hello():
             errors.append("Unable to get URL. Please make sure your url is valid and try again.")
             return render_template('index.html', errors=errors)
     
-    if resp:
-        # text processing
-        # - get only text from the html, discard others like html tags, scripts, etc.
-        raw = BeautifulSoup(resp.text, 'html.parser').get_text()
-        
-        # tokenize the raw text (into individual words)
-        nltk.data.path.append('./nltk_data/') # set the path
-        tokens = nltk.word_tokenize(raw)
-        text = nltk.Text(tokens) # turn the tokens (words) into an nltk text object
+        if resp:
+            # text processing
+            # - get only text from the html, discard others like html tags, scripts, etc.
+            raw = BeautifulSoup(resp.text, 'html.parser').get_text()
+            
+            # tokenize the raw text (into individual words)
+            nltk.data.path.append('./nltk_data/') # set the path
+            tokens = nltk.word_tokenize(raw)
+            text = nltk.Text(tokens) # turn the tokens (words) into an nltk text object
 
-        # remove punctuation, count raw words
-        nonPunct = re.compile('.*[A-Za-z].*')
-        raw_words = [w for w in text if nonPunct.match(w)]
-        raw_word_count = Counter(raw_words)
+            # remove punctuation, count raw words
+            nonPunct = re.compile('.*[A-Za-z].*')
+            raw_words = [w for w in text if nonPunct.match(w)]
+            raw_word_count = Counter(raw_words)
 
-        # stop words
-        no_stop_words = [w for w in raw_words if w.lower() not in stops]
-        no_stop_words_count = Counter(no_stop_words)
+            # stop words
+            no_stop_words = [w for w in raw_words if w.lower() not in stops]
+            no_stop_words_count = Counter(no_stop_words)
 
-        # save the results
-        results = sorted(
-            no_stop_words_count.items(),
-            key=operator.itemgetter(1),
-            reverse=True
-        )
-
-        try:
-            result = Result(
-                url=url,
-                result_all=raw_word_count,
-                result_no_stop_words=no_stop_words_count
+            # save the results
+            results = sorted(
+                no_stop_words_count.items(),
+                key=operator.itemgetter(1),
+                reverse=True
             )
-            db.session.add(result)
-            db.session.commit()
-        except:
-            errors.append("Unable to add item to database.")
+
+            try:
+                result = Result(
+                    url=url,
+                    result_all=raw_word_count,
+                    result_no_stop_words=no_stop_words_count
+                )
+                db.session.add(result)
+                db.session.commit()
+            except:
+                errors.append("Unable to add item to database.")
 
     return render_template('index.html', errors=errors, results=results)
 
